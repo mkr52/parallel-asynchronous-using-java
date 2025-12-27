d0fe80b5c20b86c971fabadfee51099dcd0875af
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-12-27 13:52:40 +0530

    added string transformation methods to ParallelStreamsExample and corresponding tests

--- src/main/java/com/learnjava/parallelstream/ParallelStreamsExample.java
+++ src/main/java/com/learnjava/parallelstream/ParallelStreamsExample.java
@@ -3,6 +3,7 @@ package com.learnjava.parallelstream;
 import com.learnjava.util.DataSet;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.learnjava.util.CommonUtil.*;
 import static com.learnjava.util.LoggerUtil.log;
@@ -12,6 +13,25 @@ public class ParallelStreamsExample {
     public List<String> stringTransform(List<String> namesList) {
         return namesList.parallelStream()
                 .map(ParallelStreamsExample::addNameLengthTransform)
+                .toList();
+    }
+
+    public List<String> stringTransform_1(List<String> namesList ,boolean isParallel) {
+        Stream<String> nameStream = namesList.stream();
+        if(isParallel)
+            nameStream.parallel();
+        return nameStream
+                .map(ParallelStreamsExample::addNameLengthTransform)
+                .toList();
+    }
+
+    // call lowercase transform function based on isParallel flag
+    public List<String> stringTransform_2(List<String> namesList ,boolean isParallel) {
+        Stream<String> nameStream = namesList.stream();
+        if(isParallel)
+            nameStream = nameStream.parallel();
+        return nameStream
+                .map(String::toLowerCase)
                 .toList();
     }
 
@@ -28,4 +48,13 @@ public class ParallelStreamsExample {
         delay(500);
         return name.length()+" - "+name ;
     }
+
+    // Create a function that takes in a list of names and
+    // transform all the elements to lowercase using parallelstreams() method.
+    private static List<String> transformToLowerCase(List<String> namesList) {
+        return namesList.parallelStream()
+                .map(String::toLowerCase)
+                .toList();
+    }
+
 }
--- src/test/java/com/learnjava/parallelstream/ParallelStreamsExampleTest.java
+++ src/test/java/com/learnjava/parallelstream/ParallelStreamsExampleTest.java
@@ -1,11 +1,13 @@
 package com.learnjava.parallelstream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static com.learnjava.util.CommonUtil.startTimer;
-import static com.learnjava.util.CommonUtil.timeTaken;
+import static com.learnjava.util.CommonUtil.*;
+import static com.learnjava.util.LoggerUtil.log;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParallelStreamsExampleTest {
@@ -29,6 +31,48 @@ class ParallelStreamsExampleTest {
         assertTrue(resultList.contains("4 - John"));
         assertTrue(resultList.contains("3 - Doe"));
         assertFalse(resultList.contains(5 + " - Alice"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {false, true})
+    void stringTransformSuccess_StreamFlag(boolean isParallel) {
+        //given
+        List<String> namesList = List.of("Adam", "Eve", "John", "Doe");
+
+        //when
+        startTimer();
+        List<String> resultList = parallelStreamsExample.stringTransform_1(namesList, isParallel);
+        timeTaken();
+
+        //then
+        assertEquals(4, resultList.size());
+        assertTrue(resultList.contains("4 - Adam"));
+        assertTrue(resultList.contains("3 - Eve"));
+        assertTrue(resultList.contains("4 - John"));
+        assertTrue(resultList.contains("3 - Doe"));
+        stopWatchReset();
+    }
+
+    // Add test for stringTransform_2 method
+    @ParameterizedTest
+    @ValueSource(booleans = {false, true})
+    void stringTransformToLowerCaseSuccess_StreamFlag(boolean isParallel) {
+        //given
+        List<String> namesList = List.of("Adam", "Eve", "John", "Doe");
+
+        //when
+        startTimer();
+        List<String> resultList = parallelStreamsExample.stringTransform_2(namesList, isParallel);
+        log("resultList : " + resultList);
+        timeTaken();
+
+        //then
+        assertEquals(4, resultList.size());
+        assertTrue(resultList.contains("adam"));
+        assertTrue(resultList.contains("eve"));
+        assertTrue(resultList.contains("john"));
+        assertTrue(resultList.contains("doe"));
+        stopWatchReset();
     }
 
 }
